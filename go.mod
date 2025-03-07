552ae7d596864640c8c9fac472be0c118e73a545
nibazshab <44338441+nibazshab@users.noreply.github.com>
2025-03-07 10:57:06 +0000

    os.Root 安全目录限制

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module sfs
 
-go 1.22.6
+go 1.24.1
--- main.go
+++ main.go
@@ -5,95 +5,95 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"path"
-	"sort"
+	"path/filepath"
+	"slices"
 	"strconv"
 	"strings"
 )
 
-var (
-	Port *string
-	Root *string
-)
+var rootfs *os.Root
 
-func main() {
-	Port = flag.String("port", "8080", "server port")
-	Root = flag.String("path", "./", "server root path")
-	flag.Parse()
-
-	fmt.Printf("start HTTP server @ 0.0.0.0:%s\nload storage @ %s", *Port, *Root)
-
-	http.HandleFunc("/", fileServer)
-	err := http.ListenAndServe(":"+*Port, nil)
-	if err != nil {
-		fmt.Printf("start error: %v", err)
-		os.Exit(1)
-	}
+type info struct {
+	name string
+	size string
+	time string
+	i    int
+	j    int
 }
 
-func fileServer(w http.ResponseWriter, req *http.Request) {
-	fPath := path.Join(*Root, req.URL.Path)
-	fObj, err := os.Open(fPath)
+func main() {
+	port := flag.String("port", "8080", "server port")
+	r := flag.String("path", "./", "server root path")
+	flag.Parse()
+
+	root, _ := filepath.Abs(*r)
+	rootfs, _ = os.OpenRoot(root)
+
+	fmt.Printf("@ 0.0.0.0:%s\n@ %s\n", *port, root)
+	http.HandleFunc("/", server)
+	http.ListenAndServe(":"+*port, nil)
+}
+
+func server(w http.ResponseWriter, req *http.Request) {
+	fmt.Printf("%s\n", req.URL.Path)
+
+	p := strings.TrimPrefix(req.URL.Path, "/")
+	if p == "" {
+		p = "."
+	}
+
+	fi, err := rootfs.Stat(p)
 	if err != nil {
 		http.NotFound(w, req)
 		return
 	}
-	defer fObj.Close()
-	fInfo, _ := fObj.Stat()
 
-	if fInfo.IsDir() {
-		fList, _ := fObj.Readdir(-1)
-		sort.SliceStable(fList, func(i, j int) bool {
-			if fList[i].IsDir() == fList[j].IsDir() {
-				return fList[i].Name() < fList[j].Name()
-			}
-			return fList[i].IsDir() && !fList[j].IsDir()
-		})
+	f, _ := rootfs.Open(p)
+	defer f.Close()
 
-		var oLastIndex string
-		oIndex := req.URL.Path
-		if oIndex == "/" {
-			oLastIndex = oIndex
-		} else {
-			oLastIndex = path.Dir(oIndex)
-			oIndex += "/"
-		}
-
-		head := fmt.Sprintf("<html><h1>Index of %s</h1><hr/><pre><a href=\"%s\">../</a>\n", oIndex, oLastIndex)
-		_, _ = w.Write([]byte(head))
-
-		for _, _f := range fList {
-			var li string
-			mName := _f.Name()
-			mUrl := path.Join(req.URL.Path, mName)
-			mTime := _f.ModTime().Format("2006-01-02 15:04:05")
-			mNameLength := max(50-len(mName), 1)
-			const mSizeLength = 19
-
-			if _f.IsDir() {
-				sn := strings.Repeat(" ", mNameLength)
-				sl := strings.Repeat(" ", mSizeLength)
-				li = fmt.Sprintf("<a href=\"%s\">%s/</a>%s%s%s-\n", mUrl, mName, sn, mTime, sl)
-			} else {
-				var mSize string
-				_size := _f.Size()
-
-				if _size > 10240 {
-					_size >>= 10
-					mSize = strconv.FormatInt(_size, 10) + "kb"
-				} else {
-					mSize = strconv.FormatInt(_size, 10)
-				}
-
-				sn := strings.Repeat(" ", mNameLength+1)
-				sl := strings.Repeat(" ", max(mSizeLength-len(mSize), 1)+1)
-				li = fmt.Sprintf("<a href=\"%s\">%s</a>%s%s%s%s\n", mUrl, mName, sn, mTime, sl, mSize)
-			}
-
-			_, _ = w.Write([]byte(li))
-		}
-		_, _ = w.Write([]byte("</pre><hr/></html>"))
-	} else {
-		http.ServeContent(w, req, fInfo.Name(), fInfo.ModTime(), fObj)
+	if !fi.IsDir() {
+		http.ServeContent(w, req, fi.Name(), fi.ModTime(), f)
+		return
 	}
+
+	fl, _ := f.Readdir(-1)
+
+	slices.SortStableFunc(fl, func(a, b os.FileInfo) int {
+		if a.IsDir() != b.IsDir() {
+			if a.IsDir() {
+				return -1
+			}
+			return 1
+		}
+		return 0
+	})
+
+	var b strings.Builder
+
+	of := "Index of " + req.URL.Path
+	b.WriteString(fmt.Sprintf("<title>%s</title><h1>%s</h1>", of, of))
+	b.WriteString("<hr><pre><a href=\"../\">../</a>\n")
+
+	var i info
+	for _, l := range fl {
+		if l.IsDir() {
+			i.name = l.Name() + "/"
+			i.size = "-"
+		} else {
+			i.name = l.Name()
+			i.size = strconv.FormatInt(l.Size(), 10)
+		}
+		i.time = l.ModTime().Format("02-Jan-2006 15:04")
+		i.i = max(51-len(i.name), 1)
+		i.j = max(20-len(i.size), 1)
+
+		b.WriteString(fmt.Sprintf("<a href=\"%s\">%s</a>", i.name, i.name))
+		b.WriteString(strings.Repeat(" ", i.i))
+		b.WriteString(i.time)
+		b.WriteString(strings.Repeat(" ", i.j))
+		b.WriteString(i.size)
+		b.WriteString("\n")
+	}
+	b.WriteString("</pre><hr>")
+	w.Write([]byte(b.String()))
 }
